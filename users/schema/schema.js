f2ed780fd60c6c00ed2189c32373f04ba7a3fcb0
Laurel Herting <laurel.herting@gmail.com>
2018-11-12 16:41:31 -0700

    replace import: lodash with axios, remove static list of users, add json-server request

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,16 +1,11 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
   GraphQLSchema
 } = graphql;
-
-const users = [
-  { id: '33', firstName: 'Jill', age: '30' },
-  { id: '43', firstName: 'Brad', age: '34' },
-];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -28,7 +23,8 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+      return axios.get(`http://localhost:3000/users/${args.id}`)
+        .then(res => res.data);
       }
     }
   }
