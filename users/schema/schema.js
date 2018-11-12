1d0bad89f7ef307a713964a0875bd5bfc98f9367
Laurel Herting <laurel.herting@gmail.com>
2018-11-12 11:36:27 -0700

    resolve with data

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,9 +1,16 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 const {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLSchema
 } = graphql;
+
+const users = [
+  { id: '33', fistName: 'Jill', age: '30' },
+  { id: '43', fistName: 'Brad', age: '34' },
+];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -12,4 +19,21 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt }
   }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType' ,
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
