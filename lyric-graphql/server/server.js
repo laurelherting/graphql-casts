09ebafa682caa7791f3e646c4ef238eab1a7fef0
Laurel Herting <laurel.herting@gmail.com>
2018-12-04 12:02:32 -0700

    connect models, schema

--- lyric-graphql/server/server.js
+++ lyric-graphql/server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const models = require('./models');
 const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const schema = require('./schema/schema');
 
 const app = express();
 
