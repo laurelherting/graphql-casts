c843448d5060f7a73b0d177a4359bf65eee3b474
Laurel Herting <laurel.herting@gmail.com>
2018-12-05 14:55:51 -0700

    add fields

--- lyric-graphql/server/schema/root_query_type.js
+++ lyric-graphql/server/schema/root_query_type.js
@@ -1,17 +1,31 @@
 const mongoose = require('mongoose');
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLList } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLString
+} = graphql;
+const Lyric = mongoose.model('lyric');
 
-const RootQuery =  new GraphQLObjectType({
-  name: 'RootQueryType',
+const LyricType = new GraphQLObjectType({
+  name:  'LyricType',
   fields: () => ({
-    songs: {
-      type: new GraphQLList(SongType),
-      resolve() {
-        return Song.find({});
+    id: { type: GraphQLID },
+    likes: { type: GraphQLInt },
+    content: { type: GraphQLString },
+    song: {
+      type: require('./song_type'),
+      resolve(parentValue) {
+        return Lyric.findById(parentValue).populate('song')
+          .then(lyric => {
+            console.log(lyric)
+            return lyric.song
+          });
       }
-    },
+    }
   })
 });
 
-module.exports = RootQuery;
+module.exports = LyricType;
