b1b0c8499c21059e8b950f27229ced472f403da4
Laurel Herting <laurel.herting@gmail.com>
2018-12-10 12:03:14 -0700

    lyric create function

---
+++ lyric-graphql/client/components/LyricCreate.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+class LyricCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { content: '' };
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+    this.props.mutate({
+      variables: {
+        content: this.state.content,
+        songId: this.props.songId
+      }
+    }).then(() => this.setState({ content: '' }));
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit.bind(this)}>
+        <label>Add a Lyric</label>
+        <input
+          value={this.state.content}
+          onChange={event => this.setState({ content: event.target.value })}
+        />
+      </form>
+    );
+  }
+};
+
+export default graphql(LyricCreate);
