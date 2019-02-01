4ee39bf649869f0d93c6e77807e4eef23bbcffeb
Laurel Herting <laurel.herting@gmail.com>
2019-01-31 18:49:23 -0700

    wip-add song details to client

---
+++ lyric-graphql/client/components/SongDetail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+
+class SongDetail extends Component {
+  render() {
+    const { song } = this.props.data;
+
+  }
+}
+
+export default graphql;
