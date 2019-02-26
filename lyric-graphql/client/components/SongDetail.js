264efe7aaf531ada4ba191250ff45b849709eae9
Laurel Herting <laurel.herting@gmail.com>
2019-02-26 11:59:20 -0700

    add songDetail info

--- lyric-graphql/client/components/SongDetail.js
+++ lyric-graphql/client/components/SongDetail.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
   render() {
     const { song } = this.props.data;
 
+    if (!song) { return <div>Loading...</div>; }
+    return ();
   }
 }
 
-export default graphql;
+export default graphql(fetchSong, {
+  options: (props) => { return { variables: { id: props.params.id } } }
+})(SongDetail);
