4cd553ec83e602dc9aa34177599c17984818e479
Laurel Herting <laurel.herting@gmail.com>
2019-02-27 16:19:03 -0700

    add import statements, update route

--- lyric-graphql/client/index.js
+++ lyric-graphql/client/index.js
@@ -4,6 +4,11 @@ import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
+import App from './components/App';
+import SongList from './components/SongList';
+import SongCreate from './components/SongCreate';
+import SongDetail from './components/SongDetail';
+
 const client = new ApolloClient({
   dataIdFromObject: o => o.id
 });
@@ -11,8 +16,13 @@ const client = new ApolloClient({
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <Route path="/" component={App}>
-      </Route>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+          <Route path="songs/new" component={SongCreate} />
+          <Route path="songs/:id" component={SongDetail} />
+        </Route>
+      </Router>
     </ApolloProvider>
   );
 };
