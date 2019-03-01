c37056201a192cd1ef8aa13903511247b62ffc96
Laurel Herting <laurel.herting@gmail.com>
2019-02-28 17:35:58 -0700

    edit return Lyric

--- lyric-graphql/client/components/SongDetail.js
+++ lyric-graphql/client/components/SongDetail.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 import fetchSong from '../queries/fetchSong';
+import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
 
 class SongDetail extends Component {
   render() {
     const { song } = this.props.data;
 
     if (!song) { return <div>Loading...</div>; }
-    return ();
+
+    return (
+      <div>
+        <Link to="/">Back</Link>
+        <h3>{song.title}</h3>
+        <LyricList lyrics={song.lyrics} />
+        <LyricCreate songId={this.props.params.id} />
+      </div>
+    );
   }
 }
 
